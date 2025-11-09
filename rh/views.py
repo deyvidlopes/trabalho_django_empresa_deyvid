029fe5b00370f93d615158118017c66cd2d7405a
vandersongc <vandersongc@gmail.com>
2025-11-09 17:49:04 -0300

    v1

---
+++ rh/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import redirect, render
+from .models import Funcionarios
+from .forms import ContatoModelForm
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+def produtos(request):
+    return render(request,'produtos.html')
+def clientes(request):
+    return render(request,'clientes.html')
+def funcionarios(request):
+    funcionarios = Funcionarios.objects.filter(status=True)
+    context = {
+        'funcionarios': funcionarios
+    }
+    return render(request,'funcionarios.html',context)
+
+# A view principal do formulário
+def formulario_contato_view(request):
+    if request.method == 'POST':
+        # Cria a instância do formulário com os dados vindos do request
+        form = ContatoModelForm(request.POST)
+        
+        if form.is_valid():
+            # A MÁGICA DO MODELFORM:
+            # form.save() cria e salva um novo objeto 'MensagemContato'
+            # no banco de dados com os dados do formulário.
+            form.save()
+            
+            # Redireciona para uma página de sucesso
+            return redirect('contato_sucesso')
+    
+    else:
+        # Se for um GET, apenas cria um formulário vazio
+        form = ContatoModelForm()
+
+    # Passa o formulário (vazio ou com erros) para o template
+    return render(request, 'contato/contatos.html', {'form': form})
+
+
+# Uma view simples para a página de "sucesso"
+def contato_sucesso_view(request):
+    return render(request, 'contato/contato_sucesso.html')
